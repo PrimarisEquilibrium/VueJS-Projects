5b05a0de63825f68f0b624996656d85b479912df
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2024-11-17 22:18:12 -0500

    feat: seperate individual todo entry logic into SFC Entry.vue

--- todo-list-app/src/App.vue
+++ todo-list-app/src/App.vue
@@ -1,9 +1,10 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
+import Entry from "./components/Entry.vue"
 
-interface TodoEntry {
-  name: string;
-  done: boolean;
+export interface TodoEntry {
+    name: string;
+    done: boolean;
 }
 
 /**
@@ -11,57 +12,57 @@ interface TodoEntry {
  * @param name The name of the todo entry.
  * @returns A TodoEntry object with the specified name and a done value initialized to false.
  */
-function createTodoEntry(name: string) : TodoEntry {
-  return {
-    name: name,
-    done: false
-  }
+function createTodoEntry(name: string): TodoEntry {
+    return {
+        name: name,
+        done: false
+    }
 }
 
 export default defineComponent({
-  data() {
-    return {
-      todo: "",
-      todoEntries: [] as TodoEntry[]
-    }
-  },
-  methods: {
-    /**
-     * Adds a todo entry to the list.
-     */
-    addEntry() : void {
-      let newTodoEntry: TodoEntry = createTodoEntry(this.todo)
-      this.todoEntries = [...this.todoEntries, newTodoEntry]
-      this.todo = ""; // Clear input box
+    data() {
+        return {
+            todo: "",
+            todoEntries: [] as TodoEntry[]
+        }
     },
+    components: {
+        Entry
+    },
+    methods: {
+        /**
+         * Adds a todo entry to the list.
+         */
+        addEntry(): void {
+            let newTodoEntry: TodoEntry = createTodoEntry(this.todo)
+            this.todoEntries = [...this.todoEntries, newTodoEntry]
+            this.todo = ""; // Clear input box
+        },
 
-    /**
-     * Deletes the specified todo entry from the list.
-     * @param entry The todo entry to be removed.
-     */
-    deleteEntry(entry: TodoEntry) : void {
-      this.todoEntries = this.todoEntries.filter(e => e !== entry)
+        /**
+         * Deletes the specified todo entry from the list.
+         * @param entry The todo entry to be removed.
+         */
+        deleteEntry(entry: TodoEntry): void {
+            this.todoEntries = this.todoEntries.filter(e => e !== entry)
+        }
     }
-  }
 })
 </script>
 
 <template>
-  <h1>Todo List App</h1>
+    <h1>Todo List App</h1>
 
-  <!-- Input for creating input entries -->
-  <form @submit.prevent="addEntry()">
-    <input type="text" placeholder="Enter todo entry" v-model="todo" required>
-    <button type="submit">Add</button>
-  </form>
+    <!-- Input for creating input entries -->
+    <form @submit.prevent="addEntry()">
+        <input type="text" placeholder="Enter todo entry" v-model="todo" required>
+        <button type="submit">Add</button>
+    </form>
 
-  <!-- Render todo entries -->
-  <div v-for="entry in todoEntries" :key="entry.name">
-    <span :class="{ done: entry.done }">{{ entry.name }}</span>
-    <input type="checkbox" v-model="entry.done">
-    <button @click="deleteEntry(entry)">X</button>
-  </div>
+    <!-- Render todo entries -->
+    <div v-for="entry in todoEntries" :key="entry.name">
+        <Entry :entry="entry" :deleteEntry="deleteEntry" />
+    </div>
 </template>
 
-<style scoped src="./styles/app.css">
-</style>
+<style scoped src="./styles/app.css"></style>
---
+++ todo-list-app/src/components/Entry.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+/**
+ * Entry Component
+ * @description Renders the information of a todo entry (TodoEntry object) and handles any individual-level operations.
+ * 
+ * - Displays the name of the todo entry.
+ * - Toggles the completion state of the task.
+ * - Deletes the todo entry when the delete button is pressed.
+ */
+
+import { defineComponent } from 'vue'
+import type { PropType } from 'vue'
+import { type TodoEntry } from '@/App.vue';
+
+export default defineComponent({
+    props: {
+        /**
+         * The TodoEntry object to use.
+         */
+        entry: {
+            type: Object as PropType<TodoEntry>,
+            required: true
+        },
+        /**
+         * A function that removes the given TodoEntry object from the TodoList.
+         */
+        deleteEntry: Function as PropType<(entry: TodoEntry) => void>
+    }
+})
+</script>
+
+<template>
+    <span :class="{ done: entry.done }">{{ entry.name }}</span>
+    <input type="checkbox" v-model="entry.done">
+    <button @click="deleteEntry(entry)">X</button>
+</template>
+
+<style scoped>
+.done {
+    text-decoration: line-through;
+}
+</style>
